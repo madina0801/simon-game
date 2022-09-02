5ab789ece5f3ca8cb12bcf49517deb7693cc4835
Madina <madinatussupova0820@gmail.com>
2022-09-02 23:12:14 +0600

    play sounds when btns are clicked

--- game.js
+++ game.js
@@ -1,28 +1,43 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
+const btns = $('.btn');
+
+// Choose a button and show it to a user
 
 function nextSequence() {
   const randomNumber = Math.trunc(Math.random() * 4);
-		
+
   const randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-		
+
+    playSound(randomChosenColor);
+    
+    return randomNumber;
+}
+
+// Play sounds when a user clicks a button
+
+btns.on('click', function() {
+  const userChosenColor = $(this).attr("id");
+  playSound(userChosenColor);
+});
+ 
+function playSound(name) {
   // Make buttons flash
-		
-  $("#" + randomChosenColor)
-		.fadeOut(100)
-		.fadeIn(100);
-		
+   $("#" + name)
+  .fadeOut(100)
+  .fadeIn(100);
   // Button sounds
-		
   let sound = new Howl({
-			src: ["sounds/" + randomChosenColor + ".mp3"],
+    src: ["sounds/" + name + ".mp3"],
   });
-		
+  
   sound.play();
-		
-		return randomNumber;
-	};
+}
+
+
+
+
 
 nextSequence();
 
