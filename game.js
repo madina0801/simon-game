2f36488161ca6c8f772f9e47677d39c4d88462b6
Madina <madinatussupova0820@gmail.com>
2022-09-04 15:16:57 +0600

    check user's answer

--- game.js
+++ game.js
@@ -1,25 +1,45 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
-const gamePattern = [];
-const btns = $('.btn');
+
+let gamePattern = [];
+let userClickedPattern = [];
+
+let started = false;
+let level = 0;
 
 // Choose a button and show it to a user
 
 function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+  $('#level-title').text(`Level ${level}`);
+
   const randomNumber = Math.trunc(Math.random() * 4);
 
   const randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
-    playSound(randomChosenColor);
-    
-    return randomNumber;
+  playSound(randomChosenColor);
 }
+
+// Start a game by pressing a key
+
+$('html').keydown(function() {
+  $('#level-title').text('Level 0');
+
+  if(!started) {
+    nextSequence();
+    started = true;
+  }
+})
 
 // Play sounds when a user clicks a button
 
-btns.on('click', function() {
+$('.btn').on('click', function() {
   const userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
 });
  
 function playSound(name) {
@@ -35,11 +55,29 @@ function playSound(name) {
   sound.play();
 }
 
+// Check the user's answer against the game sequence
 
+function checkAnswer(currentLevel) {
+  if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if(userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    } else {
+      console.log("wrong");
+      const wrongSound = new Howl({
+        src: ['sounds/wrong.mp3'],
+      });
+      wrongSound.play();
+    }
+  }
+}
 
+// Reset variables
 
-
-nextSequence();
-
-// const blueAudio = new Audio('sounds/blue.mp3');
-// blueAudio.play();
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  pressed = false;
+}
